d37185097229cedf8f565ff08d20ebd10ee2cfed
Bossun <AhmadAllan@users.noreply.github.com>
2023-03-01 17:48:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const beat = document.querySelector(".beat");
+const funTime = document.querySelector(".funTimebtn");
+const audioBeat = document.querySelector(".audioBeat");
+const auidoFun = document.querySelector(".audioFun")
+const img = document.querySelector(".press");
+
+//this function play the audio when click on beat button
+beat.addEventListener('click', () => {
+   if(audioBeat.paused){
+    audioBeat.currentTime = 0;
+    audioBeat.play();
+    img.src = "image/tootgif.gif"
+   } else{
+    audioBeat.pause();
+    img.src = "image/toot.png"
+   }
+   
+   audioBeat.onended = function(){
+    img.src = "image/toot.png"
+   }
+});
+
+
+funTime.addEventListener("mouseover", () => {
+    auidoFun.play();
+})
+  
+
+
